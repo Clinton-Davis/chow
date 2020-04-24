9653ed6cb3135096b82258bf054810e5c0427a8f
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-04-24 16:30:51 +0100

    got the mongodb talking to my flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for, request
 import os
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -12,10 +12,13 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["MONGO_DBNAME"] = "chowdown"
 
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return "Howyow!"
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("recipes.html",recipes=mongo.db.recipes.find())
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
