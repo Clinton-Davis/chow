0e1f2e799b8e8875608b1f2ef54b343cd3a625c4
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-04-29 13:11:13 +0100

    added Bcrypt and is working

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask, redirect, render_template, url_for, request, session
+from flask import Flask, redirect, render_template, flash, url_for, request, session
 import os
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
-import bcrypt
+from flask_bcrypt import Bcrypt
 if path.exists("env.py"):
   import env 
 
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["MONGO_DBNAME"] = "chowdown"
+app.config["SECRET_KEY"] = os.urandom(24)
 
 mongo = PyMongo(app)
 
@@ -20,7 +22,7 @@ mongo = PyMongo(app)
 @app.route('/all_recipe')
 def all_recipe():
   if 'username' in session:
-    return 'Hi ' + session['username']
+    return 'Hi ' + session['username'] + render_template("all_recipes.html",recipes=mongo.db.recipes.find())
   
   #This looks for all the recips in the recipein chowdown.recipes
   return render_template("all_recipes.html",recipes=mongo.db.recipes.find())
@@ -42,12 +44,12 @@ def reg():
        existing_user = users.find_one({'name': request.form['username']})
        
        if existing_user is None:
-         hashpass = bcrypt.haspw(request.form['userpassword'].encode('utf-8'), bcrypt.gensalt())
-         users.insert({'name' : request.form['username'], 'password' : hashpass})
+         pw_hash = bcrypt.generate_password_hash(request.form['userpassword']).decode('utf-8')
+         users.insert({'name' : request.form['username'], 'password' : pw_hash})
          session['username'] = request.form['username']
-         return redirect(url_for('all_recipes'))
+         return redirect(url_for('all_recipe'))
        
-       return 'That name already exists, try another'
+       return 'That name already exists, try another' + render_template('reg.html')
     return render_template('reg.html') 
      
      
@@ -80,4 +82,4 @@ if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
             port=(os.environ.get('PORT')),
             debug=True)
-    app.secret_key = 'topsecret'
+    
