86f0943277b922b76530b979fc5ba2506d62fff5
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-04-29 11:45:45 +0100

    added bcrypt and reg pathing

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, redirect, render_template, url_for, request
+from flask import Flask, redirect, render_template, url_for, request, session
 import os
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
+import bcrypt
 if path.exists("env.py"):
   import env 
 
@@ -18,8 +19,13 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/all_recipe')
 def all_recipe():
+  if 'username' in session:
+    return 'Hi ' + session['username']
+  
   #This looks for all the recips in the recipein chowdown.recipes
-    return render_template("all_recipes.html",recipes=mongo.db.recipes.find())
+  return render_template("all_recipes.html",recipes=mongo.db.recipes.find())
+  
+    
   
  
 @app.route('/recipe/<recipe_id>')
@@ -27,11 +33,41 @@ def recipe(recipe_id):
   recipe = mongo.db.recipes.find_one({'_id': ObjectId(recipe_id)})  
   return render_template('recipe.html', recipe=recipe)
   
+#Registor here and after successfull reg go to all_recipe
+@app.route('/reg', methods=['POST', 'GET'])
+def reg():
+    if request.method == 'POST':
+     #check to see if email exists
+       users = mongo.db.users
+       existing_user = users.find_one({'name': request.form['username']})
+       
+       if existing_user is None:
+         hashpass = bcrypt.haspw(request.form['userpassword'].encode('utf-8'), bcrypt.gensalt())
+         users.insert({'name' : request.form['username'], 'password' : hashpass})
+         session['username'] = request.form['username']
+         return redirect(url_for('all_recipes'))
+       
+       return 'That name already exists, try another'
+    return render_template('reg.html') 
+     
+     
+         
+
+      
+  #if not redirect to insert reg
+  #if true redirect to login
   
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
   
 @app.route('/add_recipe')
 def add__recipe():
+  #check to see if login in
+  #if not redirect to login
   return render_template('add_recipe.html')
+
 
 @app.route('/insert_recipe', methods=['POST'])
 def inset_recipe():
@@ -44,3 +80,4 @@ if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
             port=(os.environ.get('PORT')),
             debug=True)
+    app.secret_key = 'topsecret'
