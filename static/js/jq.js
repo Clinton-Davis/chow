2bf9d59fe6da9bc4a76bc987b3fce700b0b3935d
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-05-12 16:16:41 +0100

    added ripple-surface to all a tags for click animation

--- static/js/jq.js
+++ static/js/jq.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $("html").on("click", ".btn-primary", function (evt) {
+    $("html").on("click", ".ripple-surface", function (evt) {
         var btn = $(evt.currentTarget);
         var x = evt.pageX - btn.offset().left;
         var y = evt.pageY - btn.offset().top;
