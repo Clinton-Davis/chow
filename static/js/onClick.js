5d38c6aaa55055455c777a146da729bf04a61109
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-06-05 15:18:03 +0100

    added back to top button

--- static/js/onClick.js
+++ static/js/onClick.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
+  $("html").on("click", ".ripple-surface", function (evt) {
+    let btn = $(evt.currentTarget);
+    let x = evt.pageX - btn.offset().left;
+    let y = evt.pageY - btn.offset().top;
 
-    $("html").on("click", ".ripple-surface", function (evt) {
-        let btn = $(evt.currentTarget);
-        let x = evt.pageX - btn.offset().left;
-        let y = evt.pageY - btn.offset().top;
-
-        $("<span/>").appendTo(btn).css({
-            left: x,
-            top: y
-        });
+    $("<span/>").appendTo(btn).css({
+      left: x,
+      top: y,
     });
+  });
 
+  $(".btt-link").click(function (e) {
+    $(".btt-link").click(function (e) {
+      window.scrollTo(0, 0);
+    });
+  });
 });
